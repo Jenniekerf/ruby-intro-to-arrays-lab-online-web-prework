01106ffb81ed35e0b633b49ac9101409c10801f9
Jennie Kerfstedt <jenniekerf@gmail.com>
2018-12-31 16:30:48 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -6,20 +6,28 @@ def array_with_two_elements
   ["cat", "dog"]
 end
 
-def my_first_element(array)
+def first_element(array)
   array[0]
 end
 
-def my_third_element(array)
+def third_element(array)
   array[2]
 end 
 
-def my_last_element(array)
+def last_element(array)
   array[-1]
 end
 
 def first_element_with_array_methods(array)
   array.first
+end
+
+def last_element_with_array_methods(array)
+  array.last 
+end
+
+def length_of_array(array)
+  array.length 
 end
 
 def 
